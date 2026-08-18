5dd341a32e70e8c52ec3f38acddbac2236964459
shwetasonth <shwetasonth1@gmail.com>
2026-08-18 15:19:17 +0530

    completed Phase-1 part-a & part-b

--- AirbnbDemo-mern-project/app.js
+++ AirbnbDemo-mern-project/app.js
@@ -1,20 +1,26 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 const path = require("path");
 const app = express();
 const port = 8000;
 
 const Listing = require("./Models/listing.js");
 
-app.use(methodOverride("_method"));
+
 //set path
+
 app.set("view-engine", "ejs");
 app.set("views", path.join("views"));
+app.engine("ejs",ejsMate);
+
 
 //middlewares
+app.use(express.static(path.join(__dirname,"/public")))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(methodOverride("_method"));
 //connection to mongodb
 const MONGO_URL = "mongodb://127.0.0.1:27017/AirbnbDB";
 async function main() {
